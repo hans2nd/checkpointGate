312d9feeeacdfea86f224b32eed3b628e24f2913
Hans Rohana <finar.mt1@gmail.com>
2026-06-02 11:43:03 +0700

    Track Flutter source directories

--- mobile/lib/features/checkpoint/providers/checkpoint_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../data/checkpoint_repository.dart';
-
-final checkpointRepositoryProvider = Provider((ref) => CheckpointRepository());
-
-final activeCheckpointsProvider = FutureProvider.autoDispose<List<dynamic>>((ref) async {
-  final repo = ref.watch(checkpointRepositoryProvider);
-  return await repo.fetchActiveCheckpoints();
-});
