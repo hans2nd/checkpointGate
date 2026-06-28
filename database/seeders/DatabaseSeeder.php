c56d8034b9dd5f627840b1627a76ffb160853433
Hans Rohana <finar.mt1@gmail.com>
2026-06-28 21:55:54 +0700

    Update Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@checkpoint.com',
-            'password' => bcrypt('password'),
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Admin',
+        //     'email' => 'admin@checkpoint.com',
+        //     'password' => bcrypt('password'),
+        // ]);
 
         // $this->call(CheckpointSeeder::class);
         // $this->call(GateSeeder::class);
